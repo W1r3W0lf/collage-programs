eff48373f7225ff014b927a4194f55d7305c3eac
Michael Jury <6158203+W1r3W0lf@users.noreply.github.com>
2019-06-24 20:07:17 -0600

    Update birthday.go

--- birthday.go
+++ birthday.go
@@ -8,29 +8,56 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
-	"time"
+	"sort"
 )
 
 
-func bday(itter int, randFunc func(int)int)float64{
+func bday(maxNum int, randFunc func(int)int)float64{
 	days := make([]int, 365, 365)
 	grandSum := 0
 	n := 0
 	counter := 0
 
-	for i := 0 ; i < itter ; i++ {
-		for ; days[n] < 2 ; counter++{
+	for i := 0 ; i < maxNum; i++ {
+		for days[n] < 2 {
 			n = randFunc(365)
 			days[n]++
+			counter++
 		}
 		grandSum += counter
 		counter = 0
 		days = make([]int, 365, 365)
 	}
-	//result := math.Ceil(float64(grandSum)/float64(itter))
+	//result := math.Ceil(float64(grandSum)/float64(maxNum))
 	//return int(result)
-	return float64(grandSum)/float64(itter)
+	return float64(grandSum)/float64(maxNum)
 }
+
+func bdaySize(size, maxNum int, randFunc func(int)int)float64{
+	days := make([]int, 365, 365)
+	grandSum := 0
+	n := 0
+
+	for i := 0 ; i < maxNum; i++ {
+		for x:=0 ; x<size; x++{
+			n = randFunc(365)
+			days[n]++
+		}
+
+		for i:=0 ; i < 365 ; i++{
+			if days[i] > 1 {
+				grandSum++
+				break
+			}
+		}
+
+		days = make([]int, 365, 365)
+	}
+	//result := math.Ceil(float64(grandSum)/float64(maxNum))
+	//return int(result)
+	return float64(grandSum)/float64(maxNum)
+}
+
 
 func bdayQ(itter, qSize int, randFunc func(int, chan int, chan bool))float64{
 	days := make([]int, 365, 365)
@@ -44,9 +71,10 @@ func bdayQ(itter, qSize int, randFunc func(int, chan int, chan bool))float64{
 	go randFunc(365, randomIn, killer)
 
 	for i := 0 ; i < itter ; i++ {
-		for ; days[n] < 2 ; counter++{
+		for days[n] < 2 {
 			n = <- randomIn
 			days[n]++
+			counter++
 		}
 		grandSum += counter
 		counter = 0
@@ -54,6 +82,27 @@ func bdayQ(itter, qSize int, randFunc func(int, chan int, chan bool))float64{
 	}
 	//result := math.Ceil(float64(grandSum)/float64(itter))
 	//return int(result)
+	return float64(grandSum)/float64(itter)
+}
+
+func bday2(size,itter int, randFunc func(int)int)float64{
+	people := make([]int, size,size)
+	grandSum := 0
+
+	for x:=0 ; x < itter ; x++{
+		for i:=0 ; i < size ; i++{
+			people[i] = randFunc(365)
+		}
+		sort.Ints(people)
+		for i:=0 ; i < size-1 ; i++{
+			if people[i] == people[i+1]{
+				grandSum++
+				break
+			}
+		}
+		people = make([]int, size, size)
+	}
+
 	return float64(grandSum)/float64(itter)
 }
 
@@ -85,6 +134,7 @@ func devrand(max int)int{
 	return output
 }
 
+// This is too slow for practical use
 func orgrand(max int)int{
 	url := fmt.Sprintf("https://www.random.org/integers/?num=1&min=0&base=10&format=plain&rnd=new&col=1&max=%d",max-1)
 	resp, err := http.Get(url)
@@ -163,45 +213,26 @@ func orgrandQ(max int, out chan int, kill chan bool){
 
 func main() {
 
-	its := 1000
+	its := 100000
+	size := 23
 
+	randFuncNames := []string{"Default", "DevRand"}
+	randFuncs := []func(int)int{rand.Intn, devrand}
 
-	rand.Seed(time.Now().UTC().UnixNano())
+	bdayFuncNames := []string{"Original", "Other"}
+	bdayFuncs := []func(int,int,func(int)int)float64{bdaySize, bday2}
 
+	//rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(int64(devrand(100000000000000000)))
 
-	a := bday(its, rand.Intn)
-	fmt.Println("Stock ", a)
-
-	b := bday(its, devrand)
-	fmt.Println("DEV ", b)
-
-	/*
-	// This is too slow for practical use
-	c := bday(its, orgrand)
-	fmt.Println("ORG ", c)
-	*/
-
-	//qSize := 1000
-
-
-	d := bdayQ(its, 10, randQ)
-	fmt.Println("StockQ ", d)
-
-	/*
-	e := bdayQ(its, 10, devrandQ)
-	fmt.Println("DEV ", e)
-	*/
-
-	f := bdayQ(its, 1000, orgrandQ)
-	fmt.Println("OrgQ ", f)
-
-
-
-	/*
-	for x := 0 ; x < 10 ; x++{
-		//fmt.Println("DEV: ",devrand(100))
-		fmt.Println("ORG: ",orgrand(100))
+	for fun:=0 ; fun<len(randFuncs) ; fun++{
+		fmt.Println("Original", randFuncNames[fun], bday(its, randFuncs[fun]))
 	}
-	*/
 
+	for bfuni := 0 ; bfuni < len(bdayFuncs) ; bfuni++{
+		for randfuni := 0 ; randfuni < len(randFuncs) ; randfuni++{
+			fmt.Println("Size", bdayFuncNames[bfuni], randFuncNames[randfuni], bdayFuncs[bfuni](size, its, randFuncs[randfuni]))
+		}
+
+	}
 }
